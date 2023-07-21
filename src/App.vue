04476d323d32ad68d3dd99c13541652a5411f3f5
nabilapranatya <pranatyanabila.com>
2023-07-21 14:38:55 +0700

    vue-project

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,79 @@
-<script setup>
+<template>
+  <div>
+    <h2>formulir</h2>
+    <form @submit.prevent="submitForm">
+      <label for="">Nama:</label>
+      <input class="{'is-invalid' :isInvalidName}" type="text" name="nama" v-model="kontak.nama">
+      <div v-if="isInvalidName" class="error-message">Nama Harus Di isi</div>
+      <br>
+      <label for="">Email:</label>
+      <input class="{'is-invalid' :isInvalidEmail}" name="email" v-model="kontak.email">
+      <div v-if="isInvalidEmail" class="error-message">Email Harus Di Isi</div>
+      <br>
+      <input type="submit" value="Simpan">
+      <div id="input"></div>
+    </form>
+  <div v-if="showResult">
+    nama : {{ name }} <br>
+    email : {{ email }}
+  </div>
+  </div>
+</template>
+
+<script>
 import { ref } from 'vue'
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      kontak:
+      {
+        nama: '',
+        email: '',
+      },
+      showResult: ref(false),
+      name: ref(''),
+      email: ref(''),
+    };
+  },
+  computed: {
+    isInvalidName() {
+      return this.kontak.nama.length < 1;
+    },
+    isInvalidEmail() {
+      return this.kontak.email.length < 1;
+    }
+  },
 
-let msg = "MUHAMMAD"
-let rawHtml = "<h1>FAUZI</h1>"
-let link = "https://id.wikipedia.org/wiki/Pesta_Olahraga_Asia_Tenggara_2023"
-let aa = 10
-let bb = 5
-let tambah = aa + bb
-let kurang = aa - bb
 
 
+
+  methods: {
+    submitForm() {
+      this.name = this.kontak.nama;
+      this.email = this.kontak.email;
+      this.showResult = true;
+      this.isInvalidEmail = false;
+      this.isInvalidName = false;
+    },
+  },
+};
 
 </script>
 
-<template>
-  <div id ="app">
-    <span>{{ msg }}</span> <br>
-    <span v-html="rawHtml"></span> <br>
-    <a v-bind:href="link">KLIK</a> <br>
-    <p><button v-bind:disabled="isButtonDisabled" onclick="alert('disabled')">DISABLED</button></p> <br>
-    <p v-if="seen">sekarang anda bisa melihatku</p>
-    <a v-on:click="doSomething">HAI</a> <br>
+<style>
+h1 {
+  color: blue;
+}
 
-    bilangan1 = {{ aa }} <br>
-    bilangan2 = {{ bb }} <br>
-    hasil penambahan = {{ tambah }} <br>
-    hasil pengurangan = {{ kurang }}
-    
-    
-    
-    
-    </div>
-    </template>
+;
+
+.error-message {
+  color: red;
+  margin-top: 5px;
+}
+
+.is-invalid {
+  border-color: red;
+}
+</style>
